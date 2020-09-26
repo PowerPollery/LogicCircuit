6e19028801d2b842d1171d840d846efb6e0e7989
PowerPollery <pollery888@gmail.com>
2020-09-26 10:24:00 +0800

    首次提交

---
+++ src/test/Test2.java
@@ -0,0 +1,55 @@
+package test;
+
+public class Test2 {
+    public static void main(String[] args) throws InterruptedException {
+
+//        EdgeJK jk = new EdgeJK();
+//        ShowPanel sp = new ShowPanel("边沿JK触发器");
+//        sp.addInPorts(jk.get_Rd(), jk.get_Sd(), jk.getCP(), jk.getJ(), jk.getK());
+//        sp.addOutPorts(jk.getQ(), jk.get_Q());
+//        sp.showPanel();
+
+//        NotGate not = new NotGate();
+//        not.input(1);
+//        Thread.sleep(100);
+//        System.out.println(not.outPort);
+
+        //寄存器
+//        $74LS175 reg = new $74LS175();
+//        ShowPanel sp = new ShowPanel("寄存器");
+//        sp.addInPorts(reg.get_CR(), reg.getCP(), reg.getD3(), reg.getD2(), reg.getD1(), reg.getD0());
+//        sp.addOutPorts(reg.getQ3(), reg.getQ2(), reg.getQ1(), reg.getQ0());
+//        sp.showPanel();
+
+        //失败
+//        S7Decoder s7 = new S7Decoder();
+//        ShowPanel sp = new ShowPanel("7段译码器");
+//        sp.addInPorts(s7.get_LT(), s7.get_RBI(), s7.get_BIRBO(), s7.getA3(), s7.getA2(),
+//                s7.getA1(), s7.getA0());
+//        sp.addOutPorts(s7.getYa(), s7.getYb(), s7.getYc(), s7.getYd(), s7.getYe(),
+//                s7.getYf(), s7.getYg());
+//        sp.showPanel();
+
+//        A_S7Decoder s7 = new A_S7Decoder();
+//        ShowPanel sp = new ShowPanel("7段译码器");
+//        sp.addInPorts(s7.getA3(), s7.getA2(), s7.getA1(), s7.getA0());
+//        sp.addOutPorts(s7.getYa(), s7.getYb(), s7.getYc(), s7.getYd(), s7.getYe(),
+//                s7.getYf(), s7.getYg());
+//        sp.showPanel();
+
+        //模拟7段译码器
+//        S7Displayer d = new S7Displayer();
+//        ShowPanel sp = new ShowPanel();
+//        sp.addInPorts(d.getA3(), d.getA2(), d.getA1(), d.getA0());
+//        sp.showPanel();
+
+        //双向移位寄存器
+//        $74LS194 ls194 = new $74LS194();
+//        ShowPanel sp = new ShowPanel("双向移位寄存器");
+//        sp.addInPorts(ls194.get_CR(), ls194.getM1(), ls194.getM0(), ls194.getDsl(),
+//                ls194.getDsr(), ls194.getCP(), ls194.getD0(), ls194.getD1(), ls194.getD2(), ls194.getD3());
+//        sp.addOutPorts(ls194.getQ0(), ls194.getQ1(), ls194.getQ2(), ls194.getQ3());
+//        sp.showPanel();
+
+    }
+}
